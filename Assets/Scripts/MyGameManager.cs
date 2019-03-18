35dad426580ed8d04117e9108bfc9e24cb1ee704
lei.yao <yoing945@outlook.com>
2019-03-18 14:33:59 +0800

    update MainScene and some scripts

--- Assets/Scripts/MyGameManager.cs
+++ Assets/Scripts/MyGameManager.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class MyGameManager : MonoBehaviour
 {
+    public float DistancePerBlock = 1.05f;
+
     private GameState GS = GameState.Playing;
+    private ItemArray matrix;
+    private float ZIndex;
     public GameObject GO2, GO4, GO8, GO16, GO32, GO64, GO128, GO256, GO512, GO1024, GO_Blank;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateGameBoard();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //[yl] 创建棋盘
+    private void CreateGameBoard()
+    {
+        if (matrix == null)
+        {
+            for (int row = 0; row < GlobalConst.RowsNum; ++row)
+            {
+                for (int col = 0; col < GlobalConst.ColumnsNum; ++col)
+                {
+                    Instantiate(GO_Blank, this.transform.position +
+                        new Vector3(row * DistancePerBlock, col * DistancePerBlock, ZIndex), Quaternion.identity);
+                }
+            }
+        }
     }
 }
